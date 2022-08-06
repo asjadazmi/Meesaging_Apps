17874674d7a81ee8307f3f65228e2dea11244af2
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-08-06 15:31:30 +0530

    Added:Displaying the welcome message

---
+++ src/main/java/com/messagingapps/messagingapps/MessagingAppsApplication.java
@@ -0,0 +1,14 @@
+package com.messagingapps.messagingapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingAppsApplication.class, args);
+        System.out.println("welcome to the messaging apps");
+    }
+
+}
